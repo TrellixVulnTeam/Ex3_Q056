c2011ad3e5af5bae4098cbed0a31735405098e18
Guy Boucher <45304118+TackyGuy@users.noreply.github.com>
2021-03-17 00:05:24 -0400

    Suppresion de l'objet firebase

---
+++ src/data/firebase.js
@@ -0,0 +1,15 @@
+import fbAppli from 'firebase/app';
+import 'firebase/firestore';
+
+
+
+// Initialiser Firebase
+if(!fbAppli.apps.length) {
+  fbAppli.initializeApp(firebaseConfig);
+}
+
+// Initialiser Firestore
+const bd = fbAppli.firestore();
+/******* Ex#3 - Étape C ********************************/ 
+// Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
